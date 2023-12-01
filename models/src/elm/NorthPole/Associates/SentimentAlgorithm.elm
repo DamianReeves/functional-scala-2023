b099d1f5c1c198a5d7a08299100aaa89fe51c181
Damian Reeves <957246+DamianReeves@users.noreply.github.com>
2023-12-01 18:09:03 +0000

    Final update before sharing

--- models/src/elm/NorthPole/Associates/SentimentAlgorithm.elm
+++ models/src/elm/NorthPole/Associates/SentimentAlgorithm.elm
@@ -5,8 +5,6 @@ import NorthPole.Associates.Activity as Activity
 import NorthPole.Associates.Person as Person
 import NorthPole.Associates.Sentiment as Sentiment
 
-type alias Algorithm = Person.Person -> ActivityList -> Sentiment.OverallSentiment
-
 activityIsNaughty : Activity.Activity -> Bool
 activityIsNaughty activity =
   case activity of
